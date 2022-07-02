a14e605c568a221533757162ceb89250a0e53b9f
offbyone <offbyone@disroot.org>
2022-07-02 16:05:13 +0200

    Add fishing steve

--- src/main/java/com/github/selfcrafted/customlobby/Server.java
+++ src/main/java/com/github/selfcrafted/customlobby/Server.java
@@ -4,9 +4,14 @@ import com.github.selfcrafted.customlobby.commands.Commands;
 import com.github.selfcrafted.customlobby.instance.ReadOnlyAnvilLoader;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
+import net.minestom.server.entity.Entity;
+import net.minestom.server.entity.EntityType;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 import net.minestom.server.entity.damage.DamageType;
+import net.minestom.server.entity.fakeplayer.FakePlayer;
+import net.minestom.server.entity.fakeplayer.FakePlayerOption;
+import net.minestom.server.entity.metadata.other.FishingHookMeta;
 import net.minestom.server.event.entity.EntityDamageEvent;
 import net.minestom.server.event.item.ItemDropEvent;
 import net.minestom.server.event.player.*;
@@ -14,6 +19,11 @@ import net.minestom.server.extras.MojangAuth;
 import net.minestom.server.extras.bungee.BungeeCordProxy;
 import net.minestom.server.extras.velocity.VelocityProxy;
 import net.minestom.server.instance.Instance;
+import net.minestom.server.item.ItemStack;
+import net.minestom.server.item.Material;
+import net.minestom.server.network.packet.server.play.TeamsPacket;
+import net.minestom.server.scoreboard.Team;
+import net.minestom.server.scoreboard.TeamBuilder;
 import net.minestom.server.tag.Tag;
 import net.minestom.server.tag.TagHandler;
 import net.minestom.server.tag.TagSerializer;
@@ -23,6 +33,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
+import java.util.UUID;
 
 public class Server {
     public static final String VERSION = "&version";
@@ -31,6 +42,8 @@ public class Server {
 
     private static Instance INSTANCE;
     private static Pos SPAWN;
+
+    private static Team NO_NAMES_TEAM;
 
     public static void main(String[] args) throws IOException {
         System.setProperty("minestom.tps", "5");
@@ -78,12 +91,50 @@ public class Server {
             INSTANCE.setTime(-data.getTag(Tag.Long("Time")));
         } else INSTANCE.setTime(data.getTag(Tag.Long("Time")));
 
+        NO_NAMES_TEAM = new TeamBuilder("noNames", MinecraftServer.getTeamManager())
+                .nameTagVisibility(TeamsPacket.NameTagVisibility.NEVER).build();
+        TagHandler playerData = TagHandler.fromCompound(data.getTag(Tag.Structure("Player", TagSerializer.COMPOUND)));
+        var positions = playerData.getTag(Tag.Double("Pos").list());
+        double x = positions.get(0);
+        double y = positions.get(1);
+        double z = positions.get(2);
+        var look = playerData.getTag(Tag.Float("Rotation").list());
+        float lookX = look.get(0);
+        float lookY = look.get(1);
+
+        Pos playerPos = new Pos(x, y, z, lookX, lookY);
+
+        FakePlayer.initPlayer(UUID.fromString("00000000-0000-4000-8000-000000000000"), "Fishing Steve",
+                new FakePlayerOption().setInTabList(false).setRegistered(false),
+                player -> {
+                    player.setTeam(NO_NAMES_TEAM);
+                    player.setView(playerPos.yaw(), playerPos.pitch());
+                    player.getInventory().setItemInMainHand(ItemStack.of(Material.FISHING_ROD));
+
+                    // Seat
+                    Entity seat = new Entity(EntityType.BAT);
+                    seat.getEntityMeta().setInvisible(true);
+                    seat.setInstance(INSTANCE, playerPos.sub(0, 0.55, 0));
+                    seat.addPassenger(player);
+
+                    // Fishing hook
+                    Entity hook = new Entity(EntityType.FISHING_BOBBER);
+                    ((FishingHookMeta) hook.getEntityMeta()).setOwnerEntity(player);
+                    hook.setNoGravity(true);
+                    hook.setInstance(INSTANCE, player.getTargetBlockPosition(5)
+                            .add(new Pos(0.5, 0.0, 0.5)));
+                });
+
         var eventNode = MinecraftServer.getGlobalEventHandler();
         eventNode.addListener(PlayerLoginEvent.class, event -> {
             event.setSpawningInstance(INSTANCE);
             event.getPlayer().setRespawnPoint(SPAWN);
         });
-        eventNode.addListener(PlayerSpawnEvent.class, event -> event.getPlayer().setGameMode(GameMode.ADVENTURE));
+        eventNode.addListener(PlayerSpawnEvent.class, event -> {
+            if (event.getSpawnInstance() != INSTANCE) return;
+            if (event.getPlayer() instanceof FakePlayer) return;
+            event.getPlayer().setGameMode(GameMode.ADVENTURE);
+        });
         eventNode.addListener(EntityDamageEvent.class, event -> {
             if (event.getDamageType() != DamageType.VOID) return;
             if (event.getEntity() instanceof Player player) {
