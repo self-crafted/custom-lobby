46955c46d21fb5ce5094192949f7ccfa14b7e544
offbyone <offbyone@disroot.org>
2022-07-16 21:48:59 +0200

    Use slime loader for map loading

--- src/main/java/com/github/selfcrafted/customlobby/Server.java
+++ src/main/java/com/github/selfcrafted/customlobby/Server.java
@@ -1,7 +1,8 @@
 package com.github.selfcrafted.customlobby;
 
 import com.github.selfcrafted.customlobby.commands.Commands;
-import com.github.selfcrafted.customlobby.instance.ReadOnlyAnvilLoader;
+import gg.astromc.slimeloader.loader.SlimeLoader;
+import gg.astromc.slimeloader.source.SlimeSource;
 import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.Entity;
@@ -18,18 +19,18 @@ import net.minestom.server.event.player.*;
 import net.minestom.server.extras.MojangAuth;
 import net.minestom.server.extras.bungee.BungeeCordProxy;
 import net.minestom.server.extras.velocity.VelocityProxy;
-import net.minestom.server.instance.Instance;
+import net.minestom.server.instance.InstanceContainer;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
 import net.minestom.server.network.packet.server.play.TeamsPacket;
 import net.minestom.server.scoreboard.Team;
 import net.minestom.server.scoreboard.TeamBuilder;
-import net.minestom.server.tag.Tag;
-import net.minestom.server.tag.TagHandler;
-import net.minestom.server.tag.TagSerializer;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Objects;
@@ -40,7 +41,7 @@ public class Server {
     public static final String MINESTOM_VERSION = "&minestomVersion";
     private static final String START_SCRIPT_FILENAME = "start.sh";
 
-    private static Instance INSTANCE;
+    private static InstanceContainer INSTANCE;
     private static Pos SPAWN;
 
     private static Team NO_NAMES_TEAM;
@@ -78,35 +79,33 @@ public class Server {
         MinecraftServer server = MinecraftServer.init();
 
         // Create lobby instance
-        INSTANCE = MinecraftServer.getInstanceManager().createInstanceContainer(new ReadOnlyAnvilLoader());
+        INSTANCE = MinecraftServer.getInstanceManager().createInstanceContainer();
+        INSTANCE.setChunkLoader(new SlimeLoader(INSTANCE, new SlimeSource() {
+            @NotNull
+            @Override
+            public InputStream load() {
+                return Objects.requireNonNull(getClass().getResourceAsStream("/lobby.slime"),
+                        "Slime world missing!");
+            }
 
-        TagHandler data = TagHandler.fromCompound(INSTANCE.getTag(Tag.Structure("Data", TagSerializer.COMPOUND)));
-        SPAWN = new Pos(data.getTag(Tag.Integer("SpawnX")),
-                data.getTag(Tag.Integer("SpawnY")) + 1,
-                data.getTag(Tag.Integer("SpawnZ")));
+            @NotNull
+            @Override
+            public OutputStream save() {
+                return null;
+            }
+        }, true));
 
-        TagHandler gameRules = TagHandler.fromCompound(data.getTag(Tag.Structure("GameRules", TagSerializer.COMPOUND)));
-        if ("false".equals(gameRules.getTag(Tag.String("doDaylightCycle")))) {
-            INSTANCE.setTimeRate(0);
-            INSTANCE.setTime(-data.getTag(Tag.Long("Time")));
-        } else INSTANCE.setTime(data.getTag(Tag.Long("Time")));
+        SPAWN = new Pos(86, 61, 84);
+        INSTANCE.setTimeRate(0);
+        INSTANCE.setTime(-6000L);
 
         NO_NAMES_TEAM = new TeamBuilder("noNames", MinecraftServer.getTeamManager())
                 .nameTagVisibility(TeamsPacket.NameTagVisibility.NEVER).build();
-        TagHandler playerData = TagHandler.fromCompound(data.getTag(Tag.Structure("Player", TagSerializer.COMPOUND)));
-        var positions = playerData.getTag(Tag.Double("Pos").list());
-        double x = positions.get(0);
-        double y = positions.get(1);
-        double z = positions.get(2);
-        var look = playerData.getTag(Tag.Float("Rotation").list());
-        float lookX = look.get(0);
-        float lookY = look.get(1);
-
-        Pos playerPos = new Pos(x, y, z, lookX, lookY);
 
         FakePlayer.initPlayer(UUID.fromString("00000000-0000-4000-8000-000000000000"), "Fishing Steve",
                 new FakePlayerOption().setInTabList(false).setRegistered(false),
                 player -> {
+                    var playerPos = new Pos(91.0, 61.0, 89.1, 164.0F, 40.0F);
                     player.setTeam(NO_NAMES_TEAM);
                     player.setView(playerPos.yaw(), playerPos.pitch());
                     player.getInventory().setItemInMainHand(ItemStack.of(Material.FISHING_ROD));
