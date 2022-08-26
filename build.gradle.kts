2e99d188d6f060eec51c822caaf9cad6f6288b21
offbyone <offbyone@disroot.org>
2022-08-27 01:12:40 +0200

    Add TNT loader dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,10 @@ group = "com.github.selfcrafted"
 version = "1.1.0-SNAPSHOT"
 
 dependencies {
+    implementation(libs.kotlin.coroutines)
     implementation(libs.minestom)
     implementation(libs.slimeloader)
+    implementation(libs.tntloader)
 }
 
 tasks {
